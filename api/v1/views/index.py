419a5558628487b4d0d4942141693abc02512b31
Ahmed <ahmed_l2@yahoo.com>
2024-04-25 23:35:22 +0300

    3. Status of your API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,21 @@
 from api.v1.views import app_views
-from flask import jsonify
+from flask import Response
+from json import dumps
+from models import storage
 
 
 @app_views.route('/status', methods=['GET'])
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'])
+def stats():
+    data = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return Response(response=dumps(data, indent=2) + '\n', mimetype='application/json')
