1a8a40fe000a2eed055024c06bc2e533dc84fdc6
aymnn <ayman.belahsen.4@gmail.com>
2024-04-26 19:12:30 +0100

    6. State

---
+++ api/v1/views/states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import abort, jsonify
+from api.v1.views import app_views
+from models import storage
+from models.state import State
+
+
+@app_views.route('/states', methods=['GET'])
+def state():
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'])
+def one_state(state_id):
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def delete_state(state_id):
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+
