dc2a30eeaa47d97c4d5089f7299dbf73a65df165
Ahmed <ahmed_l2@yahoo.com>
2024-04-27 02:15:46 +0300

    fix

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -16,7 +16,7 @@ def state():
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def one_state(state_id):
     """Retrieves a State object"""
-    obj = storage.get(State, state_id)
+    obj = storage.get("State", state_id)
     if obj is None:
         abort(404)
     return jsonify(obj.to_dict())
@@ -26,7 +26,7 @@ def one_state(state_id):
                  strict_slashes=False)
 def delete_state(state_id):
     """Deletes a State object"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     state.delete()
@@ -38,29 +38,26 @@ def delete_state(state_id):
 def new_state():
     """Creates a new State"""
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
-
-    if 'name' not in request.get_json():
-        return make_response(jsonify({'error': 'Missing name'}), 400)
-
-    obj = State(**request.get_json())
-    obj.save()
-    return make_response(jsonify(obj.to_dict()), 201)
+        return jsonify({'error': 'Not a JSON'}), 400
+    elif 'name' not in request.get_json():
+        return jsonify({'error': 'Missing name'}), 400
+    else:
+        obj = State(**request.get_json())
+        obj.save()
+        return jsonify(obj.to_dict()), 201
 
 
 @app_views.route('/states/<string:state_id>', methods=['PUT'],
                  strict_slashes=False)
 def update_state(state_id):
     """Updates an exisiting State Object"""
-    state = storage.get(State, state_id)
+    state = storage.get("State", state_id)
     if state is None:
         abort(404)
     if not request.get_json():
-        return make_response(jsonify({'error': 'Not a JSON'}), 400)
+        return jsonify({'error': 'Not a JSON'}), 400
 
-    for key, value in request.get_json().items():
-        if key not in ['id', 'created_at', 'updated_at']:
-            setattr(state, key, value)
-
-    storage.save()
+    state_data = request.get_json()
+    state.name = state_data['name']
+    state.save()
     return jsonify(state.to_dict()), 200
