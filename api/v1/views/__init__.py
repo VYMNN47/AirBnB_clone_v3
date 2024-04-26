58ee10208604243a326305f65b80303c10a578ea
Ahmed <ahmed_l2@yahoo.com>
2024-04-26 16:27:47 +0300

    3. Status of your API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""Creates Blueprint and imports everything in the package"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
