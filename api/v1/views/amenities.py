57877f3908da763cff3edd36d9a4894045e733a0
Ahmed <ahmed_l2@yahoo.com>
2024-04-27 21:48:41 +0300

    Amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -13,7 +13,8 @@ def amenity():
     return jsonify([amenity.to_dict() for amenity in amenities.values()])
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['GET'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['GET'],
+                 strict_slashes=False)
 def single_amenity(amenity_id):
     """Returns a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
@@ -48,7 +49,8 @@ def post_amenity():
     return make_response(jsonify(amenity.to_dict()), 201)
 
 
-@app_views.route('/amenities/<amenity_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
 def put_state(amenity_id):
     """Updates a Amenity object"""
     amenity = storage.get(Amenity, amenity_id)
