de2b179919b3fea2750c8e4e615c8701196567d4
Ahmed <ahmed_l2@yahoo.com>
2024-04-27 21:54:38 +0300

    Amenity

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -3,7 +3,7 @@
 from flask import abort, jsonify, make_response, request
 from api.v1.views import app_views
 from models import storage
-from models.user import Amenity
+from models.Amenity import Amenity
 
 
 @app_views.route('/amenities', methods=['GET'], strict_slashes=False)
