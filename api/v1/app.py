eb00f16b61d2a7ab0cbbd0639f0059750a1666f3
Ahmed <ahmed_l2@yahoo.com>
2024-04-27 19:58:34 +0300

    n

--- api/v1/app.py
+++ api/v1/app.py
@@ -23,6 +23,7 @@ def error_404(error):
     '''Handles the 404 HTTP error code.'''
     return jsonify(error='Not found'), 404
 
+
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     port = os.getenv('HBNB_API_PORT', '5000')
