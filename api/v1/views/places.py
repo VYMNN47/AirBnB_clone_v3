e09ffefd731751a1716ecaf76cbb312d843d6796
Ahmed <ahmed_l2@yahoo.com>
2024-04-28 00:25:43 +0300

    fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -10,7 +10,7 @@ from models.user import User
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
-def places():
+def places(city_id):
     """Retrieves the list of all place objects in cities"""
     city = storage.get(City, city_id)
     if not city:
