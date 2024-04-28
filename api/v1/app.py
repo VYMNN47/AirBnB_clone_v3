a92a9a3c2350a8730959a2bcc68bdeaa033c3026
Ahmed <ahmed_l2@yahoo.com>
2024-04-29 02:02:02 +0300

    fix

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,7 +5,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
-from flask_cors import CORS
+
 
 app = Flask(__name__)
 """Flask web application instance."""
